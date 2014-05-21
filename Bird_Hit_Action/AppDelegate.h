ecad90e4acdc52ae56cca4eab0219751966b4b94
dongupak <dongupak@bitbucket.org>
2014-05-21 11:47:03 +0900

    first commit

---
+++ Bird_Hit_Action/AppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.h
+//  Bird_Hit_Action
+//
+//  Created by Jinhan Son on 13. 6. 27..
+//  Copyright __MyCompanyName__ 2013년. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+@interface AppController : NSObject <UIApplicationDelegate, CCDirectorDelegate>
+{
+	UIWindow *window_;
+	UINavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) UINavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
---
+++ Bird_Hit_Action/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Bird_Hit_Action
+//
+//  Created by Jinhan Son on 13. 6. 27..
+//  Copyright __MyCompanyName__ 2013년. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
